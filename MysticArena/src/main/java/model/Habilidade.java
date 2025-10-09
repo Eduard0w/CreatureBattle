8949f37444efce91d61cc4c73a1fc704fc2ac17b
Eduardo Brito <157918380+Eduard0w@users.noreply.github.com>
2025-10-08 22:43:37 -0300

    Delete MysticArena directory

--- MysticArena/src/main/java/model/Habilidade.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface Habilidade {
-	public String nomeHabilidade();
-	public TipoElemental getElemento();
-	public void atacarHabilidade(Criatura jogador, Criatura alvo);
-}
